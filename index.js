a70c9656042781d067780d1bebdfe1102a91ec4b
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-01 14:19:08 +0800

    quick

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const arr = [6, 7, 8, 5, 9, 3, 4, 1, 2];
 
-function quickSort(arr, low, high) {
-  if (low < high) {
-    let pivot = partition(arr, low, high);
-    quickSort(arr, low, pivot - 1);
-    quickSort(arr, pivot + 1, high);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let index = partition(arr, left, right);
+    quickSort(arr, left, index - 1);
+    quickSort(arr, index + 1, right);
   }
   return arr;
 }
 
-function partition(arr, low, high) {
-  let pivot = arr[low];
-  while (low < high) {
-    while (low < high && arr[high] > pivot) --high;
-    arr[low] = arr[high];
-    while (low < high && arr[low] <= pivot) ++low;
-    arr[high] = arr[low];
+function partition(arr, left, right) {
+  let pivot = arr[left];
+  while (left < right) {
+    while (left < right && arr[right] > pivot) --right;
+    arr[left] = arr[right];
+    while (left < right && arr[left] <= pivot) ++left;
+    arr[right] = arr[left];
   }
-  arr[low] = pivot;
-  return low;
+  arr[left] = pivot;
+  return left;
 }
 
 console.log(quickSort(arr));

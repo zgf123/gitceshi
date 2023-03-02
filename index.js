3f106ef02e334b28856f5fea05331061ea8da307
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-02 13:52:07 +0800

    一

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const arr = [6, 7, 8, 5, 9, 3, 4, 1, 2];
 
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
-    let index = partition(arr, left, right);
+    const index = partition(arr, left, right);
     quickSort(arr, left, index - 1);
-    quickSort(arr, index + 1, right);
+    quickSort(arr, index, right);
   }
   return arr;
 }
 
 function partition(arr, left, right) {
-  let pivot = arr[left];
-  while (left < right) {
-    while (left < right && arr[right] > pivot) --right;
-    arr[left] = arr[right];
-    while (left < right && arr[left] <= pivot) ++left;
-    arr[right] = arr[left];
+  const pivot = arr[Math.floor((left + right) / 2)];
+  let l = left;
+  let r = right;
+  while (l < r) {
+    while (arr[l] < pivot) l++;
+    while (arr[r] > pivot) r--;
+    if (l <= r) {
+      [arr[l], arr[r]] = [arr[r], arr[l]];
+      l++;
+      r--;
+    }
   }
-  arr[left] = pivot;
-  return left;
+  return l;
 }
 
 console.log(quickSort(arr));

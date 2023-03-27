04fef0f6fca0092d3a6ad3de4cc52021e834d238
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-27 14:30:21 +0800

    dui

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const arr = [6, 7, 0, 6, 12, 55, 34, -100, 999, 346, 5, 9, -3, 17, 10, -2, 1];
 
 function heapSort(arr) {
   // 创建大顶堆
-  for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
-    heapify(arr, i);
-  }
-
+  for (let i = Math.floor(arr.length / 2); i >= 0; i--) heapify(arr, i);
   const aux = [];
   while (arr.length > 1) {
     aux.unshift(arr.shift());
@@ -20,10 +17,8 @@ function heapify(arr, i) {
   const left = 2 * i + 1;
   const right = 2 * i + 2;
   let top = i;
-
   if (left < arr.length && arr[left] > arr[top]) top = left;
   if (right < arr.length && arr[right] > arr[top]) top = right;
-
   if (top !== i) {
     [arr[top], arr[i]] = [arr[i], arr[top]];
     heapify(arr, top);

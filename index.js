de57a1e4379375ad902e1cda787ce98cf19ed2a7
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-27 14:31:10 +0800

    cc

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const arr = [6, 7, 0, 6, 12, 55, 34, -100, 999, 346, 5, 9, -3, 17, 10, -2, 1];
+const arr = [6, 7, 0, 5, 9, -3, 17, 10, -2, 1];
+
+var len; // 因为声明的多个函数都需要数据长度，所以把len设置成为全局变量
 
 function heapSort(arr) {
-  // 创建大顶堆
-  for (let i = Math.floor(arr.length / 2); i >= 0; i--) heapify(arr, i);
-  const aux = [];
-  while (arr.length > 1) {
-    aux.unshift(arr.shift());
-    arr.unshift(arr.pop());
+  // 建立大顶堆
+  len = arr.length;
+  for (var i = Math.floor(len / 2); i >= 0; i--) {
+    heapify(arr, i);
+  }
+
+  for (var i = arr.length - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    len--;
     heapify(arr, 0);
   }
-  arr.push(...aux);
   return arr;
 }
 
 function heapify(arr, i) {
-  const left = 2 * i + 1;
-  const right = 2 * i + 2;
-  let top = i;
-  if (left < arr.length && arr[left] > arr[top]) top = left;
-  if (right < arr.length && arr[right] > arr[top]) top = right;
-  if (top !== i) {
-    [arr[top], arr[i]] = [arr[i], arr[top]];
-    heapify(arr, top);
+  // 堆调整
+  var left = 2 * i + 1,
+    right = 2 * i + 2,
+    largest = i;
+
+  if (left < len && arr[left] > arr[largest]) {
+    largest = left;
+  }
+
+  if (right < len && arr[right] > arr[largest]) {
+    largest = right;
+  }
+
+  if (largest != i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, largest);
   }
 }
 

1b117ccf14d5fe299422e5297abbbc085ba1ba32
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-28 18:17:07 +0800

    1

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 const arr = [6, 7, 0, 5, 9, -3, 17, 10, -2, 1];
 
-var len; // 因为声明的多个函数都需要数据长度，所以把len设置成为全局变量
-
 function heapSort(arr) {
-  // 建立大顶堆
-  len = arr.length;
-  for (var i = Math.floor(len / 2); i >= 0; i--) {
-    heapify(arr, i);
-  }
-
-  for (var i = arr.length - 1; i > 0; i--) {
-    [arr[0], arr[i]] = [arr[i], arr[0]];
-    len--;
-    heapify(arr, 0);
+  for (let i = 0; i < arr.length - 1; i++) {
+    const cur = arr[i + 1];
+    let index = i + 1;
+    for (; index > 0 && cur < arr[index - 1]; index--) {
+      arr[index] = arr[index - 1];
+    }
+    arr[index] = cur;
   }
   return arr;
-}
-
-function heapify(arr, i) {
-  // 堆调整
-  var left = 2 * i + 1,
-    right = 2 * i + 2,
-    largest = i;
-
-  if (left < len && arr[left] > arr[largest]) {
-    largest = left;
-  }
-
-  if (right < len && arr[right] > arr[largest]) {
-    largest = right;
-  }
-
-  if (largest != i) {
-    [arr[i], arr[largest]] = [arr[largest], arr[i]];
-    heapify(arr, largest);
-  }
 }
 
 console.log(heapSort(arr));

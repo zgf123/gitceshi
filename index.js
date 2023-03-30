1ee0c83f737d3a223a94e5001ffb0d11eef30604
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-30 13:32:50 +0800

    a

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const arr = [6, 7, 0, 5, 9, -3, 17, 10, -2, 1];
+const arr = [6, 7, 0, 5, 1, 9, -3, 17, 10, -2, 1];
 
-function heapSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    const cur = arr[i + 1];
-    let index = i + 1;
-    for (; index > 0 && cur < arr[index - 1]; index--) {
-      arr[index] = arr[index - 1];
-    }
-    arr[index] = cur;
-  }
+function countSort(arr) {
   return arr;
 }
 
-console.log(heapSort(arr));
+console.log(countSort(arr));
